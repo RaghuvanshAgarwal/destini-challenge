14554761280ec867bfbd60e283d5e8e8d0f7a354
Raghuvansh Agarwal <rarchva@gmail.com>
2026-06-21 19:49:21 +0530

    Challenge Complete

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,15 @@
-//TODO: Step 2 - Create a new class called Story.
+class Story {
+  late final String _storyTitle;
+  late final String _choice1;
+  late final String _choice2;
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+  String get storyTitle => _storyTitle;
+  String get choice1 => _choice1;
+  String get choice2 => _choice2;
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({
+    required this._storyTitle,
+    required this._choice1,
+    required this._choice2,
+  });
+}
